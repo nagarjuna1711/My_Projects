b3f2e15d09daa0ccb016a968778a2315f5f2ae10
Nallabothula Nagarjuna <118069056+nagarjuna1711@users.noreply.github.com>
2023-09-04 11:11:47 +0530

    Add files via upload

---
+++ HangManGame/word_list.js
@@ -0,0 +1,26 @@
+const wordList =  [
+    {
+        word: "science",
+        hint : "The scientist works by practicing of "
+    },
+    {
+        word:"bicycle",
+        hint : "A man powered vehicle."
+    },
+    {
+        word:"sunset",
+        hint:"Disappearence of the sun"
+    },
+    {
+        word:"coffee",
+        hint:"made of roasted coffee beans"
+    },
+    {
+        word:"guitar",
+        hint:"A musical instrument with strings"
+    },
+    {
+        word:"dance",
+        hint:"A rhythemic movement of body to the music."
+    }
+]
